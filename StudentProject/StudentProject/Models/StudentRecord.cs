55eca45a11580eda4604f96fa7095a19bd6320c5
Akila Peramatchi <dev.akila001@outlook.com>
2024-01-22 18:54:39 +0530

    added model and interface

---
+++ StudentProject/StudentProject/Models/StudentRecord.cs
@@ -0,0 +1,37 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentProject.Models
+
+{
+    [Table("Student_Record")]
+    public class StudentRecord
+    {
+
+        [Column("Student_id")]
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Column("Student_name")]
+        public string Name { get; set; }
+
+        [MaxLength(100)]
+        [Column("Student_mobile")]
+        public int Mobile{ get; set; }
+
+        [MaxLength(100)]
+        [Column("Student_email")]
+        public string Email { get; set; }
+
+        [MaxLength(100)]
+        [Column("Student_dateofbirth")]
+        public DateTime DateOfBirth {  get; set; }
+
+        [MaxLength(100)]
+        [Column("Student_course")]
+        public string Course {  get; set; }
+
+    }
+}
